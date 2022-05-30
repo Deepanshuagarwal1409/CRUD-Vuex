32581267684b1bea5f4cfec6ab91edbf4ba9bc3a
Deepanshu Agarwal <78287659+Deepanshuagarwal1409@users.noreply.github.com>
2022-05-30 10:33:15 +0530

    first commit.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="flex items-center justify-between flex-wrap bg-blue-900 p-6 h-20">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <router-link to="/" class="font-semibold text-xl tracking-tight"
-        >Look At My Notes</router-link
+        > CRUD DEMO</router-link
       >
     </div>
   </nav>
